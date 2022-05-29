9f4d795d0621191ba8841513a49b7f17f52bfe9a
Bratislav Bejatovic <bratislavbeat@gmail.com>
2022-05-29 23:37:06 +0200

    cube 3d

--- portfolio/src/Containers/Home/home.js
+++ portfolio/src/Containers/Home/home.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Cube from 'react-3d-cube';
 
 const Home = () => {
   return (
     <>
       <div className='home'>
-        <h1>da</h1>
+        <div>
+          <div
+            className='home_cube'
+            style={{
+              width: 300,
+              height: 300,
+            }}
+          >
+            <Cube size={300} index='front'>
+              <div>front</div>
+              <div>right</div>
+              <div>back</div>
+              <div>left</div>
+              <div>top</div>
+              <div>bottom</div>
+            </Cube>
+          </div>
+        </div>
       </div>
     </>
   );
