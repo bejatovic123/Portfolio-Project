ec9b4aedfd39cc289e438a2cdc1a7a1acf5d63cd
Bratislav Bejatovic <bratislavbeat@gmail.com>
2022-05-30 17:53:07 +0200

    Finished with Header

--- portfolio/src/components/header/Header.jsx
+++ portfolio/src/components/header/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './header.css';
+import CTA from './CTA';
+import cvpicutre from '../../assets/cvpicture.png';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
   return (
@@ -8,6 +11,14 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Bratislav Bejatovic</h1>
         <h5 className='text-light'>FrontEnd Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className='me'>
+          <img src={cvpicutre} alt='' />
+        </div>
+        <a href='#contact' className='scroll__down'>
+          Scroll Down
+        </a>
       </div>
     </header>
   );
---
+++ portfolio/src/components/header/HeaderSocials.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ImLinkedin } from 'react-icons/im';
+import { ImGithub } from 'react-icons/im';
+import { ImXing } from 'react-icons/im';
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+      <a href='https://linkedin.com' targer='_blank'>
+        <ImLinkedin />
+      </a>
+      <a href='https://github.com' targer='_blank'>
+        <ImGithub />
+      </a>
+      <a href='https://dribble.com' targer='_blank'>
+        <ImXing />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
