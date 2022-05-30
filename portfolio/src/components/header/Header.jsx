2ec203eeac57e44281d41c2ab3233c4eee0d3af9
Bratislav Bejatovic <bratislavbeat@gmail.com>
2022-05-30 16:14:05 +0200

    header

--- portfolio/src/components/header/Header.jsx
+++ portfolio/src/components/header/Header.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import './header.css';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>Bratislav Bejatovic</h1>
+        <h5 className='text-light'>FrontEnd Developer</h5>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
